2ede9c87e9b3249bb21095cf4b7181782e536f1b
NS100 <86939732+NSFivem@users.noreply.github.com>
2021-08-21 16:38:06 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,11 @@
 function text(content)
     SetTextFont(1)
     SetTextProportional(0)
-    SetTextScale(1.9,1.9)
+    SetTextScale(1.4,1.5)
     SetTextEntry("STRING")
-    SetTextComponantString(content .. "MPH")
-    DrawText(0.9,0.7)
+    AddTextComponentString(content .. "  MPH")
+    --SetTextComponantString(content .. "MPH")
+    DrawText(0.44,0.91)
 end
 
 Citizen.CreateThread(function()
@@ -12,8 +13,8 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(2)
         local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*3.6
-        if (IsPedInAnyVehicle(GetPlayerPedId(-1), false)) then
-            print('Player is in a vehicle!') --Just to make sure the script works for testing reasons
+        if (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            --print('Player is in a vehicle!') --Just to make sure the script works for testing reasons
             text(math.floor(speed))
         end
     end
